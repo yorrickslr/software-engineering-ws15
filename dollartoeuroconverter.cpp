c2dec0855774e0d8f5ec165559e67f208a4ead19
Florian Echtler <floe@butterbrot.org>
2015-10-10 23:11:11 +0200

    minor cosmetic changes

--- dollartoeuroconverter.cpp
+++ dollartoeuroconverter.cpp
@@ -7,7 +7,7 @@ dollarToEuroConverter::dollarToEuroConverter()
 }
 
 /*In: double value of dollars
- *Out: Euro value of input dollars as of 19.10.14
+ *Out: Euro value of input dollars as of 9.10.15
  */
 double dollarToEuroConverter::convert(double inputDollars){
   return inputDollars*0.88;
@@ -18,5 +18,5 @@ std::string dollarToEuroConverter::toString() const{
 }
 
 void dollarToEuroConverter::print() const{
-  std::cout << this->toString();
+  std::cout << toString();
 }
