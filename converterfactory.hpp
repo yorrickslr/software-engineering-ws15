3cb06e7d53f7def9a2b5c61c60044b2f9dc99b47
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-12-01 14:55:17 +0100

    converter factory to work on

---
+++ converterfactory.hpp
@@ -0,0 +1,33 @@
+#ifndef CONVERTERFACTORY_H
+#define CONVERTERFACTORY_H
+#include "unitconverter.hpp"
+#include <memory>
+
+
+
+
+
+class ConverterFactory
+{
+public:
+  static std::shared_ptr<ConverterFactory> instance()  {
+    if (instance_ == nullptr)
+    {
+      ConverterFactory a;
+      instance_ = make_shared<ConverterFactory>(a);
+    }
+  }
+
+  UnitConverter create(std::string const& converter_name) const{
+
+  }
+
+private:
+  ConverterFactory();
+  ~ConverterFactory();
+  static std::shared_ptr<ConverterFactory> instance_;
+};
+
+std::shared_ptr<ConverterFactory> ConverterFactory::s_instance = NULL;
+
+#endif
