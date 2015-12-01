92a8acb850adcc2c9546c3ba917722f3814dfd5f
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-12-01 16:25:47 +0100

    finished factory with prototypes :rocket:

--- converterfactory.hpp
+++ converterfactory.hpp
@@ -1,33 +1,59 @@
 #ifndef CONVERTERFACTORY_H
 #define CONVERTERFACTORY_H
-#include "unitconverter.hpp"
+#include "dollartoeuroconverter.hpp"
+#include "eurotodollarconverter.hpp"
+#include "dollartoyenconverter.hpp"
+
+#include "miletometerconverter.hpp"
+#include "yardtometerconverter.hpp"
+#include "inchtocentimeterconverter.hpp"
+ 
+#include "celsiustofahrenheitconverter.hpp"
+#include "fahrenheittocelsiusconverter.hpp"
+#include "celsiustokelvinconverter.hpp"
+
 #include <memory>
+#include <map>
 
 
 
+ConverterFactory* ConverterFactory::s_instance = NULL;
 
 
 class ConverterFactory
 {
 public:
-  static std::shared_ptr<ConverterFactory> instance()  {
+  static ConverterFactory* instance()  {
     if (instance_ == nullptr)
     {
-      ConverterFactory a;
-      instance_ = make_shared<ConverterFactory>(a);
+      instance_ = new ConverterFactory{};
     }
   }
 
-  static UnitConverter create(std::string const& converter_name) const{
-
+  static UnitConverter* create(std::string const& converter_name) const{
+    return prototypes_[converter_name]->clone();
   }
+  static ConverterFactory* instance_;
+
 
 private:
-  ConverterFactory();
+  ConverterFactory() : 
+    prototypes_{
+      {"Celsius to Fahrenheit Converter", new CelsiusToFahrenheitConverter},
+      {"Fahrenheit to Celsius Converter", new FahrenheitToCelsiusConverter},
+      {"Celsius to Kelvin Converter", new CelsiusToKelvinConverter},
+      {"Dollar to Euro Converter", new DollarToEuroConverter},
+      {"Euro to Dollar Converter", new EuroToDollarConverter},
+      {"Dollar to Yen Converter", new DollarToYenConverter},
+      {"Inch to Centimeter Converter", new InchToCentimeterConverter},
+      {"Yard to Meter Converter", new YardToMeterConverter},
+      {"Miles to Meter Converter", new MilesToMeterConverter}
+    }
   ~ConverterFactory();
-  static std::shared_ptr<ConverterFactory> instance_;
+  static std::map<std::string,UnitConverter*> prototypes_;
 };
 
-std::shared_ptr<ConverterFactory> ConverterFactory::s_instance = NULL;
+
+
 
 #endif
