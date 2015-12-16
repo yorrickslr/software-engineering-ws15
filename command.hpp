4ad5fb51c9d3f4d0fcd7442b5e4f1f4f8ff53be5
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-12-16 19:17:33 +0100

    Added command pattern

---
+++ command.hpp
@@ -0,0 +1,45 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include "dollartoeuroconverter.hpp"
+#include "eurotodollarconverter.hpp"
+#include "dollartoyenconverter.hpp"
+
+#include "miletometerconverter.hpp"
+#include "yardtometerconverter.hpp"
+#include "inchtocentimeterconverter.hpp"
+ 
+#include "celsiustofahrenheitconverter.hpp"
+#include "fahrenheittocelsiusconverter.hpp"
+#include "celsiustokelvinconverter.hpp"
+
+class Command
+{
+public:
+  typedef double (UnitConverter:: *Method) (double) const;
+  Command(UnitConverter* target, Method meth, double param) :
+    converter_{target},
+    method_{meth},
+    conv_param{param} {}
+
+  double execute() {
+    return (converter_->*method_) (conv_param);
+  }
+
+  void print_conv() {
+    converter_->print();
+  }
+
+  void print_val() {
+    std::cout << conv_param << " ";
+  }
+
+
+private:
+  UnitConverter* converter_;
+  Method method_;
+  double conv_param;
+};
+
+
+#endif
